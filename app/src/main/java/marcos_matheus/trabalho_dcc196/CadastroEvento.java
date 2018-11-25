004774da8bbdb377cf89cf2ebd502760cd30347a
Marcos <marcos-aquino@live.com>
2018-11-25 16:25:21 -0200

    Cadastro no banco de dados.

--- app/src/main/java/marcos_matheus/trabalho_dcc196/CadastroEvento.java
+++ app/src/main/java/marcos_matheus/trabalho_dcc196/CadastroEvento.java
@@ -1,13 +1,24 @@
 package marcos_matheus.trabalho_dcc196;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import marcos_matheus.trabalho_dcc196.database.DadosOpenHelper;
+import marcos_matheus.trabalho_dcc196.dominio.entidades.Evento;
+import marcos_matheus.trabalho_dcc196.dominio.repositorio.EventoRepositorio;
+import marcos_matheus.trabalho_dcc196.dominio.repositorio.InscritoRepositorio;
 
 public class CadastroEvento extends AppCompatActivity {
 
@@ -20,12 +31,26 @@ public class CadastroEvento extends AppCompatActivity {
     EditText edtFacilitadorEvento;
     EditText edtDescricaoEvento;
 
+    EventoRepositorio eventoRepositorio;
+
+    private SQLiteDatabase conexao;   // cria conexão com o banco
+
+    private DadosOpenHelper dadosOpenHelper;
+
+    private ConstraintLayout actCadEvento;
+
+    private Evento evento;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_evento);
 
-        btCadastrarEvento = (Button) findViewById(R.id.btCadastrarEvento);
+        edtTituloEvento = (EditText) findViewById(R.id.edtTituloEvento);
+        edtDataEvento = (EditText) findViewById(R.id.edtDataEvento);
+        edtHorarioEvento = (EditText) findViewById(R.id.edtHorarioEvento);
+        edtFacilitadorEvento = (EditText) findViewById(R.id.edtFacilitadorEvento);
+        edtDescricaoEvento = (EditText) findViewById(R.id.edtDescricaoEvento);
 
         btCancelar = (Button) findViewById(R.id.btCancelar);
         btCancelar.setOnClickListener(new View.OnClickListener() {
@@ -37,12 +62,7 @@ public class CadastroEvento extends AppCompatActivity {
             }
         });
 
-        edtTituloEvento = (EditText) findViewById(R.id.edtTituloEvento);
-        edtDataEvento = (EditText) findViewById(R.id.edtDataEvento);
-        edtHorarioEvento = (EditText) findViewById(R.id.edtHorarioEvento);
-        edtFacilitadorEvento = (EditText) findViewById(R.id.edtFacilitadorEvento);
-        edtDescricaoEvento = (EditText) findViewById(R.id.edtDescricaoEvento);
-
+        btCadastrarEvento = (Button) findViewById(R.id.btCadastrarEvento);
         btCadastrarEvento.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,15 +74,125 @@ public class CadastroEvento extends AppCompatActivity {
                 String data = edtDataEvento.getText().toString();
                 String hora = edtHorarioEvento.getText().toString();
 
-                if(titulo.equals("") || descricao.equals("") || facilitador.equals("") || data.equals("") || hora.equals("")) {
+                if(validaCampos()) {
                     Toast.makeText(getApplicationContext(), "Preencha todos os campos!", Toast.LENGTH_SHORT).show();
-                    finish();
+                    //finish();
                 }
                 else {
-                    Toast.makeText(getApplicationContext(), "Evento cadastrado com sucesso!", Toast.LENGTH_SHORT).show();
+                    confirmarCadastroEvento();
+                    //Toast.makeText(getApplicationContext(), "Evento cadastrado com sucesso!", Toast.LENGTH_SHORT).show();
                     finish();
                 }
             }
         });
+
+        actCadEvento = (ConstraintLayout)findViewById(R.id.actCadEvento);
+
+        criarConexao();
     }
+
+    private void criarConexao(){
+
+        try {
+
+            dadosOpenHelper = new DadosOpenHelper(this);
+
+            conexao = dadosOpenHelper.getWritableDatabase();
+
+            Snackbar.make(actCadEvento, R.string.message_conexao_criada, Snackbar.LENGTH_SHORT)
+                    .setAction(getString(R.string.action_ok), null).show();
+
+            eventoRepositorio = new EventoRepositorio(conexao);
+
+        }catch (SQLException ex){
+
+            AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+            dlg.setTitle(getString(R.string.title_erro));
+            dlg.setMessage(ex.getMessage());
+            dlg.setNeutralButton(getString(R.string.action_ok), null);
+            dlg.show();
+
+        }
+    }
+
+    private void confirmarCadastroEvento(){
+
+        evento = new Evento();
+
+        if(validaCampos() == false){
+
+            try {
+
+                eventoRepositorio.inserirEvento(evento);
+
+            }catch (SQLException ex){
+
+                AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+                dlg.setTitle(getString(R.string.title_erro));
+                dlg.setMessage(ex.getMessage());
+                dlg.setNeutralButton(getString(R.string.action_ok), null);
+                dlg.show();
+
+            }
+        }
+
+    }
+
+
+    // valida todos os campos do cadastro
+    private boolean validaCampos(){
+
+        boolean res = false;
+
+        String titulo = edtTituloEvento.getText().toString();
+        String data = edtDataEvento.getText().toString();
+        String horario = edtHorarioEvento.getText().toString();
+        String facilitador = edtFacilitadorEvento.getText().toString();
+        String descricao = edtFacilitadorEvento.getText().toString();
+
+        evento.titulo = titulo;
+        evento.data = data;
+        evento.hora = horario;
+        evento.facilitador = facilitador;
+        evento.descricao = descricao;
+
+        if(res = isCampoVazio(titulo)){
+            edtTituloEvento.requestFocus();
+        }
+        else
+        if(res = isCampoVazio(data)){
+            edtDataEvento.requestFocus();
+        }
+        else
+        if(res = isCampoVazio(horario)){
+            edtHorarioEvento.requestFocus();
+        }
+        else
+        if(res = isCampoVazio(facilitador)){
+            edtFacilitadorEvento.requestFocus();
+        }
+        else
+        if(res = isCampoVazio(descricao)){
+            edtDescricaoEvento.requestFocus();
+        }
+
+        if(res){
+
+            android.support.v7.app.AlertDialog.Builder dlg = new android.support.v7.app.AlertDialog.Builder(this);
+            dlg.setTitle(R.string.title_atencao);
+            dlg.setMessage(R.string.message_campos_nao_preenchidos);
+            dlg.setNeutralButton(R.string.lbl_ok, null);
+            dlg.show();
+        }
+
+        return res;
+    }
+
+    // verifica se campo está preenchido ou vazio
+    private boolean isCampoVazio(String valor){
+
+        boolean resultado = ( TextUtils.isEmpty(valor) || valor.trim().isEmpty() );
+        return resultado;
+    }
+
 }
--- app/src/main/java/marcos_matheus/trabalho_dcc196/CadastroParticipante.java
+++ app/src/main/java/marcos_matheus/trabalho_dcc196/CadastroParticipante.java
@@ -1,13 +1,24 @@
 package marcos_matheus.trabalho_dcc196;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import marcos_matheus.trabalho_dcc196.database.DadosOpenHelper;
+import marcos_matheus.trabalho_dcc196.dominio.entidades.Inscrito;
+import marcos_matheus.trabalho_dcc196.dominio.repositorio.InscritoRepositorio;
 
 
 public class CadastroParticipante extends AppCompatActivity {
@@ -18,6 +29,16 @@ public class CadastroParticipante extends AppCompatActivity {
     EditText edtNomeParticipante;
     EditText edtEmailParticipante;
     EditText edtCpfParticipante;
+
+    InscritoRepositorio inscritoRepositorio;
+
+    private SQLiteDatabase conexao;   // cria conexão com o banco
+
+    private DadosOpenHelper dadosOpenHelper;
+
+    private ConstraintLayout actCadParticipante;
+
+    private Inscrito inscrito;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,16 +71,117 @@ public class CadastroParticipante extends AppCompatActivity {
                 String email = edtEmailParticipante.getText().toString();
                 String cpf = edtCpfParticipante.getText().toString();
 
-                if(nome.equals("") || email.equals("") || cpf.equals("")) {
+                if(validaCampos()) {
                     Toast.makeText(getApplicationContext(), "Preencha todos os campos!", Toast.LENGTH_SHORT).show();
-                    finish();
+                    //finish();
                 } else {
-
-                    Toast.makeText(getApplicationContext(), "Participante cadastrado com sucesso!", Toast.LENGTH_SHORT).show();
+                    confirmarCadastroParticipante();
+                    //Toast.makeText(getApplicationContext(), "Participante cadastrado com sucesso!", Toast.LENGTH_SHORT).show();
                     finish();
                 }
             }
         });
 
+        actCadParticipante = (ConstraintLayout)findViewById(R.id.actCadParticipante);
+
+        criarConexao();
+    }
+
+    private void criarConexao(){
+
+        try {
+
+            dadosOpenHelper = new DadosOpenHelper(this);
+
+            conexao = dadosOpenHelper.getWritableDatabase();
+
+            Snackbar.make(actCadParticipante, R.string.message_conexao_criada, Snackbar.LENGTH_SHORT)
+                    .setAction(getString(R.string.action_ok), null).show();
+
+            inscritoRepositorio = new InscritoRepositorio(conexao);
+
+        }catch (SQLException ex){
+
+            AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+            dlg.setTitle(getString(R.string.title_erro));
+            dlg.setMessage(ex.getMessage());
+            dlg.setNeutralButton(getString(R.string.action_ok), null);
+            dlg.show();
+
+        }
+
+    }
+
+    private void confirmarCadastroParticipante(){
+
+        inscrito = new Inscrito();
+
+        if(validaCampos() == false){
+
+            try {
+
+                inscritoRepositorio.inserirInscrito(inscrito);
+
+            }catch (SQLException ex){
+
+                AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+                dlg.setTitle(getString(R.string.title_erro));
+                dlg.setMessage(ex.getMessage());
+                dlg.setNeutralButton(getString(R.string.action_ok), null);
+                dlg.show();
+
+            }
+        }
+    }
+
+    // valida todos os campos do cadastro
+    private boolean validaCampos(){
+
+        boolean res = false;
+
+        String nome = edtNomeParticipante.getText().toString();
+        String email = edtEmailParticipante.getText().toString();
+        String cpf = edtCpfParticipante.getText().toString();
+
+        inscrito.nome = nome;
+        inscrito.email = email;
+        inscrito.cpf = cpf;
+
+        if(res = isCampoVazio(nome)){
+            edtNomeParticipante.requestFocus();
+        }
+        else
+        if(res = !isEmailValido(email)){
+            edtEmailParticipante.requestFocus();
+        }
+        else
+        if(res = isCampoVazio(cpf)){
+            edtCpfParticipante.requestFocus();
+        }
+
+        if(res){
+
+            android.support.v7.app.AlertDialog.Builder dlg = new android.support.v7.app.AlertDialog.Builder(this);
+            dlg.setTitle(R.string.title_atencao);
+            dlg.setMessage(R.string.message_campos_nao_preenchidos);
+            dlg.setNeutralButton(R.string.lbl_ok, null);
+            dlg.show();
+        }
+
+        return res;
+    }
+
+    // verifica se campo está preenchido ou vazio
+    private boolean isCampoVazio(String valor){
+
+        boolean resultado = ( TextUtils.isEmpty(valor) || valor.trim().isEmpty() );
+        return resultado;
+    }
+
+    // verifica se o email é válido
+    private boolean isEmailValido(String email){
+
+        boolean resultado = ( !isCampoVazio(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches() );
+        return resultado;
     }
 }
